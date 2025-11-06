c2d02a934d28a964eb7d10ab3972bdefb49161e6
prabakaransquash <Prabhakaran@squashapps.com>
2025-11-06 12:38:02 +0530

    cors..file

--- config/cors.js
+++ config/cors.js
@@ -1,25 +1,17 @@
 const cors = require('cors');
 
-const allowedOrigins = [
-  'http://localhost:5173',
-  'https://lemon-moss-0af8f730f.1.azurestaticapps.net',
-  'http://192.168.0.54:8081',
-  'http://192.168.0.54:5000',
-  process.env.PRODUCTION_FRONTEND_URL,
-  process.env.MOBILE_APP_URL,
-].filter(Boolean);
-
+// Simplified CORS for Azure deployment
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: [
+    'http://localhost:5173',
+    'https://lemon-moss-0af8f730f.1.azurestaticapps.net',
+    'http://192.168.0.54:8081',
+    'http://192.168.0.54:5000'
+  ],
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
   credentials: true,
+  optionsSuccessStatus: 200
 };
 
 module.exports = cors(corsOptions);
